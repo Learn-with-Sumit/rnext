b75ac0a7338c8b5e760cce45b03e1d3c768abb50
Sumit Saha <sumit@learnwithsumit.com>
2024-01-02 15:36:54 +0600

    Lesson 1.4 source code import

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Gallery from "./components/Gallery";
+
+function App() {
+    return (
+        <div>
+            <Gallery />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Gallery.jsx
@@ -0,0 +1,16 @@
+function Profile() {
+    return (
+        <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
+    );
+}
+
+export default function Gallery() {
+    return (
+        <section>
+            <h1>Amazing scientists</h1>
+            <Profile />
+            <Profile />
+            <Profile />
+        </section>
+    );
+}

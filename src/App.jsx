110be97a105c7a8d4a4829c4997857b0dc566644
Sumit Saha <sumit@learnwithsumit.com>
2024-01-04 14:25:36 +0600

    lesson 1.10 source code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import PackingList from "./components/PackingList";
+const people = [
+    {
+        id: 0,
+        name: "Creola Katherine Johnson",
+        profession: "mathematician",
+    },
+    {
+        id: 1,
+        name: "Mario José Molina-Pasquel Henríquez",
+        profession: "chemist",
+    },
+    {
+        id: 2,
+        name: "Mohammad Abdus Salam",
+        profession: "physicist",
+    },
+    {
+        id: 3,
+        name: "Percy Lavon Julian",
+        profession: "chemist",
+    },
+    {
+        id: 4,
+        name: "Subrahmanyan Chandrasekhar",
+        profession: "astrophysicist",
+    },
+];
 
 export default function App() {
+    const chemists = people.filter((person) => person.profession === "chemist");
+
+    const listItems = chemists.map((chemist) => (
+        <li key={chemist.id}>{chemist.name}</li>
+    ));
+
     return (
         <div>
-            <div>
-                <PackingList />
-            </div>
+            <div>{listItems}</div>
         </div>
     );
 }

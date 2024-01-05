ca8a49b89ce6f0a0d72b943edbfe646647bb3d2f
Sumit Saha <sumit@learnwithsumit.com>
2024-01-05 15:02:38 +0600

    lesson 2.1 code

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,25 @@
-const people = [
-    {
-        id: 0,
-        name: "Creola Katherine Johnson",
-        profession: "mathematician",
-    },
-    {
-        id: 1,
-        name: "Mario José Molina-Pasquel Henríquez",
-        profession: "chemist",
-    },
-    {
-        id: 2,
-        name: "Mohammad Abdus Salam",
-        profession: "physicist",
-    },
-    {
-        id: 3,
-        name: "Percy Lavon Julian",
-        profession: "chemist",
-    },
-    {
-        id: 4,
-        name: "Subrahmanyan Chandrasekhar",
-        profession: "astrophysicist",
-    },
-];
-
 export default function App() {
-    const chemists = people.filter((person) => person.profession === "chemist");
+    return (
+        <Toolbar
+            onPlayMovie={() => alert("Playing!")}
+            onUploadImage={() => alert("Uploading!")}
+        />
+    );
+}
 
-    const listItems = chemists.map((chemist) => (
-        <li key={chemist.id}>{chemist.name}</li>
-    ));
-
+function Toolbar({ onPlayMovie, onUploadImage }) {
     return (
         <div>
-            <div>{listItems}</div>
+            <Button onSmash={onPlayMovie}>Play Movie</Button>
+            <Button onSmash={onUploadImage}>Upload Image</Button>
         </div>
+    );
+}
+
+function Button({ onSmash, children }) {
+    return (
+        <p>
+            <button onClick={onSmash}>{children}</button>
+        </p>
     );
 }

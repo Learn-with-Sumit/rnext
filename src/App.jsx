8debfa6f332c290dd9118b8429c63d57e0e46522
Sumit Saha <sumit@learnwithsumit.com>
2024-01-05 16:03:52 +0600

    lesson 2.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
-export default function App() {
+export default function Signup() {
     return (
-        <Toolbar
-            onPlayMovie={() => alert("Playing!")}
-            onUploadImage={() => alert("Uploading!")}
-        />
-    );
-}
-
-function Toolbar({ onPlayMovie, onUploadImage }) {
-    return (
-        <div>
-            <Button onSmash={onPlayMovie}>Play Movie</Button>
-            <Button onSmash={onUploadImage}>Upload Image</Button>
-        </div>
-    );
-}
-
-function Button({ onSmash, children }) {
-    return (
-        <p>
-            <button onClick={onSmash}>{children}</button>
-        </p>
+        <form
+            onSubmit={(e) => {
+                e.preventDefault();
+                alert("Submitting!");
+            }}
+        >
+            <input />
+            <button>Send</button>
+        </form>
     );
 }

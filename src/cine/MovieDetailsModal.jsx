7c39b64932b4bbc4f2753a7054b8bd1c885446c0
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-01-11 16:15:30 +0530

    Implemented movie details page

---
+++ src/cine/MovieDetailsModal.jsx
@@ -0,0 +1,50 @@
+import { getImgUrl } from "../utils/cine-utility";
+
+export default function MovieDetailsModal({movie, onClose, onCartAdd}) {
+    return (
+        <div className="fixed top-0 left-0 w-screen h-screen z-50 bg-black/60 backdrop-blur-sm">
+            <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[420px] sm:max-w-[600px] lg:max-w-[984px] p-4 max-h-[90vh] overflow-auto">
+                <div className="bg-white shadow-md dark:bg-[#12141D] rounded-2xl sm:grid sm:grid-cols-[2fr_1fr] overflow-hidden">
+                    <img
+                        className="sm:order-2 w-full object-cover h-full max-sm:max-h-[300px]"
+                        src={getImgUrl(
+                            `../assets/movie-covers/${movie.cover}`
+                        )}
+                        alt=""
+                    />
+                    <div className="p-5 lg:p-11">
+                        <div className="">
+                            <h2 className="text-3xl lg:text-[50px] mb-2 font-bold">
+                                {movie.title}
+                            </h2>
+                            <span className="block text-base text-[#9fa0a4] dark:text-[#575A6E] my-3">
+                                {movie.genre}
+                            </span>
+                            <div></div>
+                        </div>
+                        <p className="text-sm lg:text-base mb-8 lg:mb-16">
+                            {movie.description}
+                        </p>
+                        <div className="grid lg:grid-cols-2 gap-2">
+                            <a
+                                className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+                                href="#"
+                                onClick={(e) => onCartAdd(e, movie)}
+                            >
+                                <img src="./assets/tag.svg" alt="" />
+                                <span>${movie.price} | Add to Cart</span>
+                            </a>
+                            <a
+                                className="border border-[#74766F] rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#6F6F6F] dark:text-gray-200 font-semibold text-sm"
+                                href="#"
+                                onClick={onClose}
+                            >
+                                Cancel
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/cine/MovieList.jsx
+++ src/cine/MovieList.jsx
@@ -1,42 +1,70 @@
+import { useState } from "react";
+
 import { getAllMovies } from "../data/movies";
+import MovieDetailsModal from "./MovieDetailsModal";
 import Rating from "./Rating";
 
 import { getImgUrl } from "../utils/cine-utility";
 
 export default function MovieList() {
+    const [showModal, setShowModal] = useState(false);
+    const [selectedMovie, setSelectedMovie] = useState(null);
+
     const movies = getAllMovies();
+
+    function hadleMovieSelection(movie) {
+        setSelectedMovie(movie);
+        setShowModal(true);
+    }
+
+    function hadleAddToCart(event, movie) {
+        event.stopPropagation();
+        console.log("Add to Cart", movie);
+    }
+
+    function handleModalClose() {
+        setSelectedMovie(null);
+        setShowModal(false);
+    }
 
     return (
         <div className="content">
+            {showModal && <MovieDetailsModal
+                movie={selectedMovie}
+                onClose={handleModalClose}
+                onCartAdd={hadleAddToCart} />}
             <div className="grid sm:grid-cols-2 xl:grid-cols-3 gap-7">
                 {movies.map((movie) => (
                     <figure
                         key={movie.id}
                         className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl"
                     >
-                        <img
-                            className="w-full object-cover"
-                            src={getImgUrl(
-                                `../assets/movie-covers/${movie.cover}`
-                            )}
-                            alt={movie.title}
-                        />
-                        <figcaption className="pt-4">
-                            <h3 className="text-xl mb-1">{movie.title}</h3>
-                            <p className="text-[#575A6E] text-sm mb-2">
-                                {movie.genre}
-                            </p>
-                            <div className="flex items-center space-x-1 mb-5">
-                                <Rating value={movie.rating} />
-                            </div>
-                            <a
-                                className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-                                href="#"
-                            >
-                                <img src="./assets/tag.svg" alt="" />
-                                <span>$100 | Add to Cart</span>
-                            </a>
-                        </figcaption>
+                        <a href="#" onClick={() => hadleMovieSelection(movie)}>
+                            <img
+                                className="w-full object-cover"
+                                src={getImgUrl(
+                                    `../assets/movie-covers/${movie.cover}`
+                                )}
+                                alt={movie.title}
+                            />
+                            <figcaption className="pt-4">
+                                <h3 className="text-xl mb-1">{movie.title}</h3>
+                                <p className="text-[#575A6E] text-sm mb-2">
+                                    {movie.genre}
+                                </p>
+                                <div className="flex items-center space-x-1 mb-5">
+                                    <Rating value={movie.rating} />
+                                </div>
+                                <button
+                                    className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+                                    href="#"
+                                    onClick={(e) => hadleAddToCart(e, movie)}
+                                >
+                                    <img src="./assets/tag.svg" alt="" />
+                                    <span>${movie.price} | Add to Cart</span>
+                                </button>
+                            </figcaption>
+                        </a>
                     </figure>
                 ))}
             </div>

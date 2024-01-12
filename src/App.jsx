670856a2997742b6735008f4fb90e572ccc1a11d
Sumit Saha <sumit@learnwithsumit.com>
2024-01-12 14:45:33 +0600

    lesson 3.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import Form from "./components/Form";
 import "./styles.css";
 
-const statuses = ["empty", "typing", "submitting", "success", "error"];
-
 function App() {
     return (
         <div>
-            {statuses.map((status) => (
-                <Form key={status} status={status} />
-            ))}
+            <Form />
         </div>
     );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,33 +1,68 @@
-// eslint-disable-next-line react/prop-types
-export default function Form({ status = "empty" }) {
-    // visual states: empty, typing, submitting, success, error
+import { useState } from "react";
 
-    if (status === "success")
-        return (
-            <>
-                <h1>Thats right!</h1>
-                <hr />
-            </>
-        );
+function submitForm(answer) {
+    // Pretend it's hitting the network.
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (answer.toLowerCase() === "dhaka") {
+                resolve();
+            } else {
+                reject(new Error("Good guess but a wrong answer. Try again!"));
+            }
+        }, 3000);
+    });
+}
+
+// eslint-disable-next-line react/prop-types
+export default function Form() {
+    // visual states: typing, submitting, success
+
+    // mandatory data state
+    const [answer, setAnswer] = useState("");
+    const [error, setError] = useState(null);
+
+    // visual state theke paoa final state
+    const [status, setStatus] = useState("typing");
+
+    if (status === "success") return <h1>Thats right!</h1>;
+
+    // handlers
+    const handleTextChange = (e) => {
+        setError(null);
+        setAnswer(e.target.value);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setStatus("submitting");
+
+        try {
+            await submitForm(answer);
+            setStatus("success");
+        } catch (err) {
+            setStatus("typing");
+            setError(err.message);
+        }
+    };
 
     return (
         <>
             <h2>City quiz</h2>
             <p>What city is located on two continents?</p>
-            <form id="form">
-                <textarea disabled={status === "submitting"}></textarea>
+            <form onSubmit={handleSubmit}>
+                <textarea
+                    value={answer}
+                    onChange={handleTextChange}
+                    disabled={status === "submitting"}
+                ></textarea>
                 <br />
-                <button
-                    disabled={status === "empty" || status === "submitting"}
-                >
+                <button disabled={answer === "" || status === "submitting"}>
                     Submit
                 </button>
                 {status === "submitting" && <p>Loading...</p>}
-                {status === "error" && (
-                    <p className="Error">There was an error</p>
-                )}
+
+                {error && <p className="Error">{error}</p>}
             </form>
-            <hr />
         </>
     );
 }

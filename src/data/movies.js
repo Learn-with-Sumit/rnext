0188279bb944c2d03aaf89b113c221a4418e3f98
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-01-13 08:06:12 +0530

    (fix) a small typo

--- src/data/movies.js
+++ src/data/movies.js
@@ -4,7 +4,7 @@ const data = [
         "cover": "once-in-ho.jpg",
         "title": "Once Upon a Time... in Hollywood",
         "description": "A faded television actor and his stunt double strive to achieve fame and success in the film industry during the final years of Hollywood's Golden Age in 1969 Los Angeles.",
-        "genre": "Cpmedy/Drama",
+        "genre": "Comedy/Drama",
         "rating": 5,
         "price": 140
     },

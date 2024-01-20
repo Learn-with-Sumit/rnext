84aec0c7680351e40bb094d0c7dbf87be089221f
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-01-20 08:11:35 +0530

    Added tailwindCSS styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            container: {
+                center: true,
+                padding: "1.25rem",
+            }
+        },
+    },
+    plugins: [],
+};

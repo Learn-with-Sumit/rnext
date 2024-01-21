ecf25cf8c14b5340527a80503c8b74d1935d58ce
Sumit Saha <sumit@learnwithsumit.com>
2024-01-21 14:55:16 +0600

    changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Stopwatch from "./components/Counter";
+
+function App() {
+    return (
+        <div>
+            <Stopwatch />
+        </div>
+    );
+}
+
+export default App;

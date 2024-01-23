7ad117f8ae970918dbbb7d09bef65286d4eff1f9
Sumit Saha <sumit@learnwithsumit.com>
2024-01-23 14:04:04 +0600

    changes

--- src/components/MyInput.jsx
+++ src/components/MyInput.jsx
@@ -1,7 +1,15 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const MyInput = function (props, ref) {
-    return <input {...props} ref={ref} />;
+    const realInputRef = useRef(null);
+
+    useImperativeHandle(ref, () => ({
+        focus() {
+            realInputRef.current.focus();
+        },
+    }));
+
+    return <input {...props} ref={realInputRef} />;
 };
 
 const ForwardedMyInput = forwardRef(MyInput);

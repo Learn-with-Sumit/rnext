d28bf11b8ec76a2f11e713aa662a32d518260ac7
Saad Hasan <saadh393@gmail.com>
2024-01-23 14:19:21 +0600

    Fixed Bug

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
 
     const getNextId = (data) => {
         const maxId = data.reduce((prev, current) =>
-            prev && prev.id > current.id ? prev.id : current.id
+            prev && prev.id > current.id ? prev : current.id, 0
         );
 
         return maxId + 1;

34827843f509eb0d7d28b97341cf402372a48124
Sumit Saha <sumit@learnwithsumit.com>
2024-02-04 16:19:21 +0600

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,9 @@
-import { useState } from "react";
-import Page from "./components/Page";
-import ShoppingCartContext from "./contexts/shoppingCartContext";
-
-const items = [
-    {
-        id: 1,
-        title: "Product 1",
-    },
-];
+import Pointer from "./components/Pointer";
 
 export default function App() {
-    const [page, setPage] = useState("/home");
-    const [cartItems, setCartItems] = useState(items);
-
-    const handlePageChange = () => {
-        setPage("/cart");
-    };
-
-    const addItem = () => {
-        setCartItems([
-            ...cartItems,
-            {
-                id: 2,
-                title: "Product 2",
-            },
-        ]);
-    };
-
     return (
         <div>
-            <ShoppingCartContext.Provider value={cartItems}>
-                <Page
-                    url={page}
-                    onPageChange={handlePageChange}
-                    onAddItems={addItem}
-                />
-            </ShoppingCartContext.Provider>
+            <Pointer />
         </div>
     );
 }

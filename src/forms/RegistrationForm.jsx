3c56b402b4a3cc660630dc839b8f6bba599792e1
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-02-04 11:10:19 +0530

    Committed Registraion

--- src/forms/RegistrationForm.jsx
+++ src/forms/RegistrationForm.jsx
@@ -11,7 +11,7 @@ const RegistrationForm = () => {
         handleSubmit,
         formState: { errors },
         setError,
-        control
+        control,
     } = useForm();
 
     const { fields, append, remove } = useFieldArray({
@@ -27,6 +27,16 @@ const RegistrationForm = () => {
         <div className="flex flex-col justify-center items-center">
             <form onSubmit={handleSubmit(submitForm)}>
                 <FieldSet label="Enter Your Basic Details">
+                    <Field label="Picture" error={errors.picture}>
+                        <input
+                            {...register("picture", {
+                                required: "Picture is required",
+                            })}
+                            type="file"
+                            id="picture"
+                            name="picture"
+                        />
+                    </Field>
                     <Field label="Email" error={errors.email}>
                         <input
                             {...register("email", {
@@ -83,33 +93,38 @@ const RegistrationForm = () => {
                     </Field>
 
                     <Field label="Age" error={errors.age}>
-                       <Controller
+                        <Controller
                             name="age"
                             control={control}
-                            render={ ({field: {ref, ...field}}) => (
+                            defaultValue={1}
+                            render={({ field: { ref, ...field } }) => (
                                 <NumberInput
                                     id="age"
                                     className={`p-2 border box-border w-full rounded-md ${
-                                        !!errors.age ? "border-red-500" : "border-gray-200"
-                                      }`}
-                                      {...field} />
+                                        !!errors.age
+                                            ? "border-red-500"
+                                            : "border-gray-200"
+                                    }`}
+                                    {...field}
+                                />
                             )}
                             rules={{
                                 max: {
                                     value: 100,
-                                    message: "Age can be between 0 and 100"
-                                }
-                            }} />
+                                    message: "Age can be between 0 and 100",
+                                },
+                            }}
+                        />
                     </Field>
                 </FieldSet>
 
                 <FieldSet label="Enter Social Handles">
-                    {
-                       fields.map((field, index) => {
-                        return(
+                    {fields.map((field, index) => {
+                        return (
                             <div
-                            className="flex justify-between items-center w-max"
-                            key={field.id}>
+                                className="flex justify-between items-center w-max"
+                                key={field.id}
+                            >
                                 <Field label="Social Name">
                                     <input
                                         className="p-2 border box-border w-full rounded-md"
@@ -131,16 +146,17 @@ const RegistrationForm = () => {
                                 </Field>
                                 <button
                                     className="mt-8 mr-2 text-2xl"
-                                    onClick={() => remove(index)}>
+                                    onClick={() => remove(index)}
+                                >
                                     &#8722;
                                 </button>
                             </div>
-                        )
-                       })
-                    }
+                        );
+                    })}
                     <button
-                    className="mt-8 text-md text-white cursor-pointer border rounded-lg bg-gray-500 p-1 m-auto"
-                    onClick={() => append({name: "", url:""})}>
+                        className="mt-8 text-md text-white cursor-pointer border rounded-lg bg-gray-500 p-1 m-auto"
+                        onClick={() => append({ name: "", url: "" })}
+                    >
                         Add A Social Handle
                     </button>
                 </FieldSet>

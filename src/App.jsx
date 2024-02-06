cbb5eec6a24e3cf54009700e24211c589a92f017
Sumit Saha <sumit@learnwithsumit.com>
2024-02-06 21:52:41 +0600

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
-import { useState } from "react";
-import "./App.css";
-import Welcome from "./components/Welcome";
+import { useCallback, useMemo, useState } from "react";
+import Button from "./components/Button";
+import ShowCount from "./components/ShowCount";
+import Title from "./components/Title";
 
-export default function App() {
-    const [show, setShow] = useState(false);
+function App() {
+    const [count1, setCount1] = useState(0);
+    const [count2, setCount2] = useState(0);
+
+    const incrementByOne = useCallback(() => {
+        setCount1((prevCount) => prevCount + 1);
+    }, []);
+
+    const incrementByFive = useCallback(() => {
+        setCount2((prevCount) => prevCount + 5);
+    }, []);
+
+    const isEven = useMemo(() => {
+        let i = 0;
+        while (i < 1000000000) {
+            i += 1;
+        }
+
+        return count1 % 2 === 0;
+    }, [count1]);
+
     return (
-        <>
-            <button onClick={() => setShow((s) => !s)}>
-                {show ? "Remove" : "Show"}
-            </button>
+        <div className="app">
+            <Title />
+            <ShowCount count={count1} title="Counter 1" />
+            <span>{isEven ? "Even" : "Odd"}</span>
 
+            <Button handleClick={incrementByOne}>Increment by one</Button>
             <hr />
-
-            {show && <Welcome />}
-        </>
+            <ShowCount count={count2} title="Counter 2" />
+            <Button handleClick={incrementByFive}>Increment by five</Button>
+        </div>
     );
 }
+
+export default App;

9ee005081cb55c313bc3748a218eb487f8b27e83
Sumit Saha <sumit@learnwithsumit.com>
2024-02-09 18:14:54 +0600

    changes

--- api/main.js
+++ api/main.js
@@ -13,21 +13,21 @@ app.get("/v2/search", search);
 
 // Not Found Middleware
 app.use((req, res, next) => {
-  res.status(404).send("Page not found");
+    res.status(404).send("Page not found");
 });
 
 // Error Middleware
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something went wrong");
+    console.error(err.stack);
+    res.status(500).send("Something went wrong");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000\n");
-  console.log("Available Enpoints:");
+app.listen(8000, () => {
+    console.log("Server is running on port 8000\n");
+    console.log("Available Enpoints:");
 
-  const host = "http://localhost:3000";
+    const host = "http://localhost:8000";
 
-  console.log(`GET ${host}/v2/top-headlines?category=general\n`);
-  console.log(`GET ${host}/v2/search?q=Develop`);
+    console.log(`GET ${host}/v2/top-headlines?category=general\n`);
+    console.log(`GET ${host}/v2/search?q=Develop`);
 });

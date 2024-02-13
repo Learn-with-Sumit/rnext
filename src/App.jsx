3735402daf44307c99385b0df49da4476f2779fd
Sumit Saha <sumit@learnwithsumit.com>
2024-02-13 14:11:03 +0600

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,13 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import Comments from "./components/Comments";
 import PostSelector from "./components/PostSelector";
 
 export default function App() {
-    const [posts, setPosts] = useState([]);
-    const [selectedPostId, setSelectedPostId] = useState(null);
-    const [isPostsLoading, setIsPostsLoading] = useState(false);
-    const [postsError, setPostsError] = useState(null);
-
-    const handleSelectPost = (e) => {
-        setSelectedPostId(e.target.value);
-    };
-
-    useEffect(() => {
-        setIsPostsLoading(true);
-        setPostsError(null);
-
-        const fetchPosts = async () => {
-            try {
-                const response = await fetch(
-                    "https://jsonplaceholder.typicode.com/posts?_limit=5"
-                );
-                const data = await response.json();
-
-                if (response.ok) {
-                    setIsPostsLoading(false);
-                    setPosts(data);
-                } else {
-                    setIsPostsLoading(false);
-                    setPostsError("There was an error");
-                }
-            } catch (err) {
-                setIsPostsLoading(false);
-                setPostsError(err.message);
-            }
-        };
-
-        fetchPosts();
-    }, []);
-
-    // choose what to render for postsSelector
-    let postsContent;
-
-    if (isPostsLoading) {
-        postsContent = <div>Posts loading...</div>;
-    } else if (!isPostsLoading && postsError) {
-        postsContent = <div className="error">{postsError}</div>;
-    } else {
-        postsContent = (
-            <PostSelector posts={posts} onSelectPost={handleSelectPost} />
-        );
-    }
-
     return (
         <div>
             <h1>React Suspense and Error Boundaries</h1>
 
             <div>
-                {postsContent}
-
-                {selectedPostId && <Comments postId={selectedPostId} />}
+                <PostSelector />
             </div>
         </div>
     );
--- src/components/PostSelector.jsx
+++ src/components/PostSelector.jsx
@@ -1,12 +1,67 @@
-export default function PostSelector({ posts, onSelectPost }) {
+import { useEffect, useState } from "react";
+import Comments from "./Comments";
+
+export default function PostSelector() {
+    const [posts, setPosts] = useState([]);
+    const [selectedPostId, setSelectedPostId] = useState(null);
+    const [isPostsLoading, setIsPostsLoading] = useState(false);
+    const [postsError, setPostsError] = useState(null);
+
+    const handleSelectPost = (e) => {
+        setSelectedPostId(e.target.value);
+    };
+
+    useEffect(() => {
+        setIsPostsLoading(true);
+        setPostsError(null);
+
+        const fetchPosts = async () => {
+            try {
+                const response = await fetch(
+                    "https://jsonplaceholder.typicode.com/posts?_limit=5"
+                );
+                const data = await response.json();
+
+                if (response.ok) {
+                    setIsPostsLoading(false);
+                    setPosts(data);
+                } else {
+                    setIsPostsLoading(false);
+                    setPostsError("There was an error");
+                }
+            } catch (err) {
+                setIsPostsLoading(false);
+                setPostsError(err.message);
+            }
+        };
+
+        fetchPosts();
+    }, []);
+
+    // choose what to render for postsSelector
+    let postsContent;
+
+    if (isPostsLoading) {
+        postsContent = <div>Posts loading...</div>;
+    } else if (!isPostsLoading && postsError) {
+        postsContent = <div className="error">{postsError}</div>;
+    } else {
+        postsContent = (
+            <select onChange={handleSelectPost}>
+                <option value="">Select Post</option>
+                {posts.map((post) => (
+                    <option key={post.id} value={post.id}>
+                        {post.title}
+                    </option>
+                ))}
+            </select>
+        );
+    }
+
     return (
-        <select onChange={onSelectPost}>
-            <option value="">Select Post</option>
-            {posts.map((post) => (
-                <option key={post.id} value={post.id}>
-                    {post.title}
-                </option>
-            ))}
-        </select>
+        <div>
+            {postsContent}
+            {selectedPostId && <Comments postId={selectedPostId} />}
+        </div>
     );
 }

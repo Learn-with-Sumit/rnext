c77f52b4d69c003c1216cd3c2a6c09bc54e42e1e
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-03-02 19:13:03 +0530

    Initial commits

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1
+        className="text-3xl text-center text-red-700"
+      >Welcome to Vite with TailwindCSS and React</h1>
+    </>
+  )
+}
+
+export default App

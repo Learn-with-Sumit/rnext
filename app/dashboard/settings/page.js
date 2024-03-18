c1fb9c4c452a07ddb8d7e919fb5b25db40b0a2d2
Sumit Saha <sumit@learnwithsumit.com>
2024-03-18 12:15:48 +0600

    changes

---
+++ app/dashboard/settings/page.js
@@ -0,0 +1,14 @@
+export default function Settings() {
+    return (
+        <div>
+            <h1 className="text-2xl">Settings page</h1>
+
+            <p className="mt-10">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laboriosam accusantium dolor at veniam doloribus, delectus
+                repellendus iste. Quis explicabo voluptatem in ea nisi! Pariatur
+                molestiae tenetur reiciendis, atque tempore quis!
+            </p>
+        </div>
+    );
+}

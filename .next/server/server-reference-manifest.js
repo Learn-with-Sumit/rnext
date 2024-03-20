9f9b2ae442e97eb628a8248f3b8d3ad3ffbe4ddf
Sumit Saha <sumit@learnwithsumit.com>
2024-03-20 20:22:06 +0600

    changes

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"MGsvGZmQ8Y1ICkery6sI3DRc/zyrKUn4QcMykt5U9RU=\"}"
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html", "./dist/productPage.html", "./dist/category.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

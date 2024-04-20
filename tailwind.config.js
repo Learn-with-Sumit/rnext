ecd4745ae451cd96f16f9001517f7a3a59a4467b
Sumit Saha <sumit@learnwithsumit.com>
2024-04-20 14:17:42 +0600

    changes

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html", "./dist/details.html", "./dist/categorized.html", "./dist/login.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

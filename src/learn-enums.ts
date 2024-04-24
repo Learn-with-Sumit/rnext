80f56144504a707612f5b43c9f5904aac8898ac0
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-04-24 19:41:21 +0530

    enums typescript done

---
+++ src/learn-enums.ts
@@ -0,0 +1,18 @@
+enum Status1 {
+    NOT_STARTED = 0,
+    IN_PROGRESS = 1,
+    COMPLETED = 2
+}
+
+
+enum Status {
+    NOT_STARTED,
+    IN_PROGRESS,
+    COMPLETED
+}
+
+enum Status2 {
+    NOT_STARTED = "Not Started",
+    IN_PROGRESS = "In Progress",
+    COMPLETED = "Completed"
+}

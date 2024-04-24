38c331c05bee857d94781f9ee0f27a7a9994f9c4
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-04-24 19:23:45 +0530

    typescript array

---
+++ src/learn-array.ts
@@ -0,0 +1,7 @@
+let names: string[] = ['lws', 'tapascript']
+
+let ages: Array<number> = [12, 56, 78,]
+
+let positions: (string | number)[] = [1, 'first', 'last', 100]
+
+let multidimArr: number[][] = [[1,2,3,4,5], [8,9,10,11,12]];

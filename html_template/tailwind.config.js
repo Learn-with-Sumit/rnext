8847ead4c672c778e563ba8f432bf3fc17128761
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-04-25 11:28:21 +0530

    Committing the initial structure

---
+++ html_template/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6A28",
+      },
+    },
+  },
+  plugins: [],
+};

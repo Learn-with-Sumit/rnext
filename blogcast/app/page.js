884d060c26eeb2feebaab90ed199ba608e80fd27
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-09-24 14:37:06 +0530

    (feat) home page style

--- blogcast/app/page.js
+++ blogcast/app/page.js
@@ -1,7 +1,21 @@
-import PostList from "./components/posts/post-list";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>BlogCast</div>
+    <div className="flex relative flex-col justify-center items-center h-screen">
+        <div className="p-2 flex flex-col justify-center items-center bg-gray-100 lg:w-2/5 h-1/4 rounded">
+          <div className="flex pb-5 mb-5 text-5xl text-back">
+            <h2>BlogCast</h2>
+          </div>
+          <div>
+            <Link
+              href="/blog"
+              className="bg-black text-white hover:bg-black/90 p-3 rounded-sm text-lg text mr-2"
+            >
+              TAKE ME TO THE BLOG
+            </Link>
+          </div>
+			</div>
+  </div>
   );
 }

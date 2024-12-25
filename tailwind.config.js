f5f438af4c4b0909d61c9d7508120a50b05700d1
Sumit Saha <sumit@learnwithsumit.com>
2024-12-25 14:59:15 +0600

    changes

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#009688",
+      },
+    },
+  },
+};
